327299df064ec079b23cd08e69bc979f2525e88e
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-26 15:22:19 +0300

    app.py index validate

--- app.py
+++ app.py
@@ -4,25 +4,24 @@ from forms import CourseForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '76bc3254cacc9d8d5d8e5461451b460e70dc4c8db9ee3d2d'
 
-
 courses_list = [{
     'title': 'Python 101',
     'description': 'Learn Python basics',
     'price': 34,
     'available': True,
     'level': 'Beginner'
-    }]
-
+}]
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
     form = CourseForm()
     if form.validate_on_submit():
         courses_list.append({
-            'title': form.title.date,
-            'description': form.description.date,
-            'available': form.available.date,
-            'level': form.level.date
+            'title': form.title.data,
+            'description': form.description.data,
+            'price': form.price.data,
+            'available': form.available.data,
+            'level': form.level.data
         })
         return redirect(url_for('courses'))
     return render_template('index.html', form=form)
